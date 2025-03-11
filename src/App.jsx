3880c1cfdeb49f38752c9495e0efe0826c930468
SushmitaThapaMagar <sushmitachan44@gmail.com>
2025-03-11 11:58:43 +0545

    add

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+
+const App = () => {
+  
+
+  return (
+
+
+<BrowserRouter>
+<Routes>
+  <Route path='/' element = {<h2>Home Page</h2>}/>
+
+
+</Routes>
+</BrowserRouter>
+
+
+  )
+}
+
+
+export default App
