1d4b72075dbc5f4cf07badc2de888fe169de6b9c
SushmitaThapaMagar <sushmitachan44@gmail.com>
2025-03-13 19:56:15 +0545

    add

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from '../components/NavBar'
+import Card from '../components/Card'
+import axios from 'axios'
 const Home = () => {
+const[books, setBooks] = useState([])
+const fetchBooks =() =>{
+
+  axios.get('http://localhost:3000/book')
+}
+useEffect(()=>{
+
+},[])
+
   return (
     <>
     < Navbar/>
+    <div className='flex flex-wrap justify-evenly mt-25'>
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    </div>
     </>
   )
 }
