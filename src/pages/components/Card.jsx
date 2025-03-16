7a4e6c06232161b43774b627d846deae534230a7
SushmitaThapaMagar <sushmitachan44@gmail.com>
2025-03-16 13:13:21 +0545

    add

--- src/pages/components/Card.jsx
+++ src/pages/components/Card.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Card = ({book}) => {
+    const navigate=useNavigate()
     console.log(book)
   return (
     
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 mb-10">
+<div class="max-w-sm bg-white border border-gray-100 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 mb-10">
     <a href="#">
-        <img class="rounded-t-lg" src="https://images-cdn.ubuy.co.in/664dcf28471622150a10a1af-the-alchemist-a-graphic-novel-hardcover.jpg" alt="" />
+        <img class="rounded-t-lg" src={book.imageUrl ? book.imageUrl : "https://fr.web.img3.acsta.net/pictures/21/08/04/17/39/2012034.jpg"} alt="" />
     </a>
     <div class="p-5">
         <a href="#">
         </a>
         <div class="font-bold text-xl mb-2">{book.bookName}</div>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">The global orange market was valued at approximately USD 3.8 billion in 2024 and is projected to grow at a compound annual growth rate (CAGR) of about 4.30% to reach USD 5.8 billion by 2034.</p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Read more
-             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-            </svg>
-        </a>
+        <p class="text-gray-700 text-base">Rs. {book.bookPrice}</p>
+        <p class="text-black-700 text-base">ISBN no. : {book.isbnNumber}</p>
+    
+        <Link to={`/book/${book._id}`}>See More</Link>
+           
+ 
+
     </div>
 </div>
 
--- src/pages/singleBook/SingleBook.jsx
+++ src/pages/singleBook/SingleBook.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import NavBar from '../components/NavBar'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 const SingleBook = () => {
+const {id} = useParams()
+const [book, setBook]=useState({})
+console.log(id)
+
+const fetchBook = async()=>{
+  const response = await axios.get(`http://localhost:3000/book/${id}`)
+  if(response.status===200){
+    setBook(response.data.data)
+  }
+}
+useEffect(()=>{
+  fetchBook()
+},[])
+console.log(book)
+
   return (
-    <div>SingleBook</div>
+    <>
+    <NavBar />
+    <div class="max-w-sm bg-white border border-gray-100 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 mb-20">
+    <a href="#">
+    <img class="rounded-t-lg" src={book.imageUrl ? book.imageUrl : "https://fr.web.img3.acsta.net/pictures/21/08/04/17/39/2012034.jpg"} alt="" />
+    </a>
+    <div class="p-5">
+        <a href="#">
+        </a>
+        <div class="font-bold text-xl mb-2">{book.bookName}</div>
+        <p class="text-gray-700 text-base">Rs.{book.bookPrice}</p>
+        <p class="text-black-700 text-base">ISBN no. : {book.isbnNumber} </p>
+        <p class="text-black-700 text-base">Author Name : {book.authorName} </p>
+        <p class="text-black-700 text-base">Published Date : {book.publishAt} </p>
+        <p class="text-black-700 text-base">Publication : {book.publication} </p>
+
+
+    </div>
+</div>
+
+    </>
   )
 }
 
